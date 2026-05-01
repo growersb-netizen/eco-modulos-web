826bb908bdb7f2689d84d420071d16eaeb6d8b70
Eco Modulos <growersb@gmail.com>
2026-05-01 01:48:44 -0300

    feat: ImageUpload dual mode (file + URL) + allow external image domains

--- next.config.ts
+++ next.config.ts
@@ -8,18 +8,29 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.r2.dev',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.cloudflarestorage.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pub-*.r2.dev',
-      },
+      // Cloudflare R2
+      { protocol: 'https', hostname: '*.r2.dev' },
+      { protocol: 'https', hostname: '*.cloudflarestorage.com' },
+      { protocol: 'https', hostname: 'pub-*.r2.dev' },
+      // Google Drive / Workspace
+      { protocol: 'https', hostname: 'drive.google.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: '*.googleusercontent.com' },
+      // Dropbox
+      { protocol: 'https', hostname: '*.dropboxusercontent.com' },
+      { protocol: 'https', hostname: 'dl.dropboxusercontent.com' },
+      // ImgBB
+      { protocol: 'https', hostname: 'i.ibb.co' },
+      // Imgur
+      { protocol: 'https', hostname: 'i.imgur.com' },
+      // WhatsApp / Meta CDN
+      { protocol: 'https', hostname: '*.fbcdn.net' },
+      { protocol: 'https', hostname: '*.cdninstagram.com' },
+      // Cloudinary
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      // GitHub
+      { protocol: 'https', hostname: 'raw.githubusercontent.com' },
+      { protocol: 'https', hostname: '*.githubusercontent.com' },
     ],
   },
   experimental: {
